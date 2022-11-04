13fb949bff8e9baaaa81ef942deab7139d8864c3
SAShamark <sashamark7777@gmail.com>
2022-11-04 22:40:17 +0200

    Lab 8(1) ended

---
+++ Lab8/Lab8(1)/Lab8/Garage.cs
@@ -0,0 +1,36 @@
+﻿namespace Lab8;
+
+public class Garage
+{
+    public List<Car> Cars { get; } = new();
+
+    public void Init()
+    {
+        AddCar(new Car("Audi", "Orange", "250", "2003"));
+        AddCar(new Car("Audi", "Black", "350", "2020"));
+        AddCar(new Car("Nissan", "Orange", "300", "2010"));
+        AddCar(new Car("Mustang", "White", "450", "2022"));
+    }
+
+    public void AddCar(Car car)
+    {
+        Cars.Add(car);
+    }
+
+    public void RemoveCar(Car car)
+    {
+        Cars.Remove(car);
+    }
+
+    public void ShowAutoPark()
+    {
+        Console.WriteLine("Your car park:");
+        for (var i = 0; i < Cars.Count; i++)
+        {
+            Console.WriteLine($"Car index - {i}");
+            Cars[i].GetCarData();
+        }
+    }
+    
+    
+}
---
+++ Lab8/Lab8(1)/Lab8/Program.cs
@@ -0,0 +1,110 @@
+﻿namespace Lab8
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            var garage = new Garage();
+            garage.Init();
+            garage.ShowAutoPark();
+
+
+            Console.WriteLine("Do you want to buy or cast out one of the cars?");
+            Console.WriteLine(" Buy - 1 \n Cast out - 2");
+            var solution = int.Parse(Console.ReadLine());
+            switch (solution)
+            {
+                case 1:
+                    BuyCar(garage);
+                    break;
+                case 2:
+                    CostOutCar(garage);
+                    break;
+            }
+
+            GetCarForDrive(garage.Cars);
+        }
+
+        private static void BuyCar(Garage garage)
+        {
+            Console.WriteLine("By what index do you want to throw away the car?");
+
+            Console.WriteLine("Chose car brand");
+            string carBrand = Console.ReadLine();
+
+            Console.WriteLine("Chose car color");
+            string carColor = Console.ReadLine();
+
+            Console.WriteLine("Chose car speed");
+            string carSpeed = Console.ReadLine();
+
+            Console.WriteLine("Chose car year manufacture");
+            string carYearManufacture = Console.ReadLine();
+
+            var newCar = new Car(carBrand, carColor, carSpeed, carYearManufacture);
+            garage.AddCar(newCar);
+            garage.ShowAutoPark();
+        }
+
+        private static void CostOutCar(Garage garage)
+        {
+            Console.WriteLine("By what index do you want to throw away the car?");
+            int carIndex = int.Parse(Console.ReadLine());
+            garage.RemoveCar(garage.Cars[carIndex]);
+            garage.ShowAutoPark();
+        }
+
+        private static void GetCarForDrive(List<Car> cars)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter the parameters of the car you want to drive");
+                var parameters = Console.ReadLine();
+                var carsByParameter = SearchCarByParameters(parameters, cars);
+
+                switch (carsByParameter.Count)
+                {
+                    case 0:
+                    {
+                        Console.WriteLine("According to the entered parameters, the car was not found");
+                        continue;
+                    }
+
+                    case 1:
+                        Console.WriteLine("You take car: ");
+                        carsByParameter[0].GetCarData();
+                        Console.WriteLine("Have a nice trip!");
+                        break;
+                    case >= 2:
+                    {
+                        Console.WriteLine("Several machines were found according to the entered parameters:");
+                        foreach (var car in carsByParameter)
+                        {
+                            car.GetCarData();
+                        }
+
+                        cars = carsByParameter;
+                        continue;
+                    }
+                }
+
+                break;
+            }
+        }
+
+        private static List<Car> SearchCarByParameters(string parameters, List<Car> cars)
+        {
+            var carsByParameters = new List<Car>();
+            foreach (var car in cars)
+            {
+                if (parameters == car.BrandCar || parameters == car.ColorCar || parameters == car.SpeedCar ||
+                    parameters == car.YearManufactureCar)
+                {
+                    carsByParameters.Add(car);
+                }
+            }
+
+            return carsByParameters;
+        }
+    }
+}
