9c351bd48db268ede5e389583b0ece1a5fd29aab
SAShamark <sashamark7777@gmail.com>
2022-11-06 00:33:38 +0200

    Lab9(2) ended

---
+++ Lab9/Lab9(2)/Lab9/Program.cs
@@ -0,0 +1,34 @@
+﻿using Lab9.Triangles;
+
+namespace Lab9
+{
+    public static class Program
+    {
+        private static int _numberTriangle;
+
+        private static void Main()
+        {
+            var picture = new Picture(3);
+            picture.Init();
+
+            picture.GetInfoAboutTriangles();
+            Console.WriteLine($"1- add triangle\n" +
+                              $"2- check triangle area for remove");
+            int action = int.Parse(Console.ReadLine());
+            switch (action)
+            {
+                case 1:
+                    picture.AddTriangle(new IsTriangle($"is{_numberTriangle}"));
+                    _numberTriangle++;
+                    break;
+                case 2:
+                    Console.WriteLine("Write triangle name");
+                    string name = Console.ReadLine();
+                    picture.CheckAreaTriangleForMove(name);
+                    break;
+            }
+
+            picture.GetInfoAboutTriangles();
+        }
+    }
+}
---
+++ Lab9/Lab9(2)/Lab9/Triangles/Triangle.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+
+namespace Lab9.Triangles
+{
+    public abstract class Triangle
+    {
+        protected Random Rand { get; } = new();
+        private int Max => byte.MaxValue + 1; //256
+        public abstract string Name { get; }
+        public abstract Color TriangleColor { get; set; }
+        public abstract int SideLength { get; set; }
+
+        public abstract int OtherSideLength { get; set; }
+
+        public abstract int AngleBetweenSides { get; set; }
+        public abstract double TriangleArea { get; set; }
+        public abstract double TrianglePerimeter { get; set; }
+        protected Color GetRandomColor()
+        {
+            int r = Rand.Next(Max);
+            int g = Rand.Next(Max);
+            int b = Rand.Next(Max);
+            var color = Color.FromArgb(r, g, b);
+            return color;
+        }
+
+        public abstract void AreaCalculation();
+
+        public abstract void PerimeterCalculation();
+    }
+}
